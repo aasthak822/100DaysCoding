a062a1ea54adafdfcfcd298fa68540dc5a9a2111
unknown <aasthakumari811@gmail.com>
2022-11-23 20:01:01 +0530

    AreaOfCircle

---
+++ Day001.java
@@ -0,0 +1,12 @@
+/*To Enter radius of the circle and Calculate the area  */
+
+import java.util.*;
+
+public class circle {
+	public static void main(String []args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Ënter Radius");
+		int r = sc.nextInt();
+		System.out.println("Area is : "+ 3.14*r*r);
+	}
+}
