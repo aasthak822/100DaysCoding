ad30bb397a7cea7e4335ffbb2f5c2bff50e7d134
unknown <aasthakumari811@gmail.com>
2022-12-03 18:46:00 +0530

    Convert Binary number To Decimal number

---
+++ Day016.java
@@ -0,0 +1,28 @@
+/* Convert Binary number To Decimal number
+SOLUTION */  
+
+
+
+public class BinaryToDecimal {
+	
+	public static void binToDec (int binNum) {
+		int pow=0;
+		int decNum=0;
+		
+		while(binNum>0) {
+			
+			int lastDigit=binNum%10;
+			decNum=decNum+(lastDigit*(int)Math.pow(2,pow));
+			
+			pow++;
+			binNum=binNum/10;
+			
+		}
+		System.out.print("The decimal of the "+binNum+" is "+decNum);
+	}
+	
+	public static void main(String args[]) {
+		binToDec(101);
+	}
+
+}
