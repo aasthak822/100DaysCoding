10d025ead51fa0e393855ccb1b02b64b0d758d6b
unknown <aasthakumari811@gmail.com>
2023-02-01 22:47:00 +0530

    Balanced Brackets in an expression

---
+++ Day076.java
@@ -0,0 +1,71 @@
+/* 
+
+Write a program to check for Balanced Brackets in an expression(well -formedness) using stack.
+
+SOLUTION */
+
+
+import java.util.*;
+ 
+public class BalancedBrackets {
+ 
+    // function to check if brackets are balanced
+    static boolean areBracketsBalanced(String expr)
+    {
+        // Using ArrayDeque is faster than using Stack class
+        Deque<Character> stack
+            = new ArrayDeque<Character>();
+ 
+        // Traversing the Expression
+        for (int i = 0; i < expr.length(); i++) {
+            char x = expr.charAt(i);
+ 
+            if (x == '(' || x == '[' || x == '{') {
+                // Push the element in the stack
+                stack.push(x);
+                continue;
+            }
+ 
+            // If current character is not opening
+            // bracket, then it must be closing. So stack
+            // cannot be empty at this point.
+            if (stack.isEmpty())
+                return false;
+            char check;
+            switch (x) {
+            case ')':
+                check = stack.pop();
+                if (check == '{' || check == '[')
+                    return false;
+                break;
+ 
+            case '}':
+                check = stack.pop();
+                if (check == '(' || check == '[')
+                    return false;
+                break;
+ 
+            case ']':
+                check = stack.pop();
+                if (check == '(' || check == '{')
+                    return false;
+                break;
+            }
+        }
+ 
+        // Check Empty Stack
+        return (stack.isEmpty());
+    }
+ 
+  
+    public static void main(String[] args)
+    {
+        String expr = "([{}])";
+ 
+        
+        if (areBracketsBalanced(expr))
+            System.out.println("Balanced ");
+        else
+            System.out.println("Not Balanced ");
+    }
+}
