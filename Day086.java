b38b8fe24ef3201cddb5cc7863b50aae7c43a3c2
unknown <aasthakumari811@gmail.com>
2023-02-11 19:44:56 +0530

    Activity Selection Problem

---
+++ Day086.java
@@ -0,0 +1,43 @@
+/* Greedy Algorithm
+
+Activity Selection Problem 
+
+SOLUTION */
+
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+ 
+class ActivitySelection {
+   
+    public static void printMaxActivities(int s[], int f[],
+                                          int n)
+    {
+        int i, j;
+ 
+        System.out.println(
+            "Following activities are selected");
+ 
+        i = 0;
+        System.out.print(i + " ");
+ 
+        for (j = 1; j < n; j++) {
+           
+
+            if (s[j] >= f[i]) {
+                System.out.print(j + " ");
+                i = j;
+            }
+        }
+    }
+ 
+    public static void main(String[] args)
+    {
+        int s[] = { 1, 3, 0, 5, 8, 5 };
+        int f[] = { 2, 4, 6, 7, 9, 9 };
+        int n = s.length;
+ 
+        printMaxActivities(s, f, n);
+    }
+}
