040e62b77d07f2e67096bbb1b814f6f6196a65d7
unknown <aasthakumari811@gmail.com>
2023-02-20 22:47:16 +0530

    Tree traversal postOrder

---
+++ Day095.java
@@ -0,0 +1,61 @@
+/* Tree traversal postOrder 
+
+Algorithm Postorder(tree)
+
+Traverse the left subtree, i.e., call Postorder(left->subtree)
+Traverse the right subtree, i.e., call Postorder(right->subtree)
+Visit the root
+
+SOLUTION */
+
+class Node {
+    int key;
+    Node left, right;
+ 
+    public Node(int item)
+    {
+        key = item;
+        left = right = null;
+    }
+}
+ 
+class BinaryTree {
+   
+    Node root;
+ 
+    BinaryTree() { root = null; }
+ 
+    
+    void printPostorder(Node node)
+    {
+        if (node == null)
+            return;
+ 
+      
+        printPostorder(node.left);
+ 
+
+        printPostorder(node.right);
+ 
+        
+        System.out.print(node.key + " ");
+    }
+ 
+    
+    void printPostorder() { printPostorder(root); }
+ 
+   
+    public static void main(String[] args)
+    {
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+ 
+        System.out.println(
+            "\nPostorder traversal of binary tree is ");
+        tree.printPostorder();
+    }
+}
