5762a5c351d494bc698872bd4982bc72741a25cc
unknown <aasthakumari811@gmail.com>
2023-02-24 22:27:17 +0530

    Diameter of a Binary Tree

---
+++ Day099.java
@@ -0,0 +1,78 @@
+/* Diameter of a Binary Tree 
+
+The diameter of a tree T is the largest of the following quantities:
+the diameter of T’s left subtree.
+the diameter of T’s right subtree.
+the longest path between leaves that goes through the root of T (this can be computed from the heights of the subtrees of T)
+
+SOLUTION */
+
+class Node {
+
+    int data;
+    Node left, right;
+ 
+    public Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}
+ 
+
+
+class BinaryTree {
+    Node root;
+ 
+   
+    int diameter(Node root)
+    {
+        if (root == null)
+            return 0;
+ 
+    
+        int lheight = height(root.left);
+        int rheight = height(root.right);
+ 
+      
+        int ldiameter = diameter(root.left);
+        int rdiameter = diameter(root.right);
+ 
+      
+        return Math.max(lheight + rheight + 1,
+                        Math.max(ldiameter, rdiameter));
+    }
+ 
+  
+    int diameter() { return diameter(root); }
+ 
+    
+    static int height(Node node)
+    {
+        
+        if (node == null)
+            return 0;
+ 
+      
+        return (1
+                + Math.max(height(node.left),
+                           height(node.right)));
+    }
+ 
+    
+    public static void main(String args[])
+    {
+        
+        BinaryTree tree = new BinaryTree();
+        tree.root = new Node(1);
+        tree.root.left = new Node(2);
+        tree.root.right = new Node(3);
+        tree.root.left.left = new Node(4);
+        tree.root.left.right = new Node(5);
+ 
+        
+        System.out.println(
+            "The diameter of given binary tree is : "
+            + tree.diameter());
+    }
+}
